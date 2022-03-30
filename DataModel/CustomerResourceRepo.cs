b2ce95d3506d4703a6d03bbe405d3ab8a95e3dd0
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2022-03-30 10:02:49 +0100

    Q1 c done

---
+++ DataModel/CustomerResourceRepo.cs
@@ -0,0 +1,83 @@
+﻿using Mock2021DataLayer.DataModel;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+
+namespace RadMock2021.DataModel
+{
+    /* Remember to impalment which can be done by
+    (right click when underlined in red to implement
+     after writing the class name and the I... parts)
+     */
+
+    public class CustomerResourceRepo : IResource, ICustomer
+    {
+
+        // create the subscription Db context
+        // which is already set up make sure that the subscription is public
+
+        Subscription_DB_Context subscriptionDB;
+
+        public CustomerResourceRepo(Subscription_DB_Context subscriptionDB)
+        {
+ 
+            this.subscriptionDB = subscriptionDB;
+        }
+
+        public Customer Get(int id)
+        {  // use the subscription to access the customer and add a return
+            // find custoemer by ID
+            return subscriptionDB.Customers.Find(id);
+
+        }
+
+        public ICollection<Customer> GetCustomerList()
+        {
+            // get will get all customers
+            return subscriptionDB.Customers.ToList();
+        }
+
+        public Customer getCustomerPurchaseHistory(int customerID)
+        {
+            // create a customer
+            Customer result = new Customer();
+            // select the customer via id
+            var SelectedCustomer = Get(customerID);
+            result = SelectedCustomer;
+            // add customers purchase history to a list.
+            result.PurchaseHisories = SelectedCustomer.PurchaseHisories.ToList();
+            return result;
+            //throw new System.NotImplementedException();
+        }
+
+        public Customer getCustomerTopupHistory(int customerID)
+        {
+            // create a customer
+            Customer result = new Customer();
+            // select the customer via id
+            var SelectedCustomer = Get(customerID);
+            result = SelectedCustomer;
+            // add customers TopUp History to a list.
+            result.TopUpHistories = SelectedCustomer.TopUpHistories.ToList();
+            // return the customer
+            return result;
+            //throw new system.notimplementedexception();
+        }
+
+        public OnlineResource GetResouceDetails(int id)
+        {
+            // finds the Online Resouce by customers id
+            return subscriptionDB.OnlineResources.Find(id);
+            //throw new System.NotImplementedException();
+        }
+
+        public ICollection<OnlineResource> GetResoursesList()
+        {
+            // get will get all the Online resources
+            return subscriptionDB.OnlineResources.ToList();
+
+            //throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DataModel/ICustomer.cs
@@ -0,0 +1,33 @@
+﻿using Mock2021DataLayer.DataModel;
+using System.Collections.Generic;
+
+namespace RadMock2021.DataModel
+{
+    // make sure it is public
+    public interface ICustomer
+    {
+        // get customer Id
+        Customer Get(int id);
+
+        // use ICollection for the getting all the customers in a  list
+        ICollection<Customer> GetCustomerList();
+
+        // get topup history using customer id which is forigen key in that table.
+        Customer getCustomerTopupHistory(int customerID);
+
+        // get purchase history using customer id which is forigen key in that table.
+
+        Customer getCustomerPurchaseHistory(int customerID);
+    }
+
+    // IResource class (make public) this is for our online resouce
+    public interface IResource
+    {
+        // get online resouce details using PK which is Resource ID.
+        OnlineResource GetResouceDetails(int id);
+        
+        // use ICollection for listing the all online resource
+        ICollection<OnlineResource> GetResoursesList();
+
+    }
+}
